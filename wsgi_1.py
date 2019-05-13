aaea5931ac0464e41a7022c4a68f2bce2bb148a1
smitco <courtjsmith11@gmail.com>
2019-05-12 18:19:08 -0700

    Lesson 04 wsgi book app exercise

--- wsgi_1.py
+++ wsgi_1.py
@@ -14,6 +14,7 @@ body = """<html>
 </body>
 </html>"""
 
+now = datetime.date.today()
 
 def application(environ, start_response):
     import pprint
@@ -21,11 +22,11 @@ def application(environ, start_response):
 
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('PATH_INFO', default),
+        month=now.strftime("%B"),
+        date=now.day,
+        year=now.year,
+        client_ip=environ.get('REMOTE_ADDR', default)
     )
     status = '200 OK'
 
